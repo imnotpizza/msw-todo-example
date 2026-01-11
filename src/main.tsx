eb7dab2d7ac8faddba8409c6053faf22bbd5d134
IMNOTPIZZA <48155178+imnotpizza@users.noreply.github.com>
2026-01-11 19:47:49 +0900

    Fix: msw 초기화 시 에러 발생 문제 수정
    No known conditions for "./ClientRequest" specifier in "@mswjs/interceptors" package
    
    - initMsw 실행 후 초기화되도록 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,32 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { startMsw } from './mocks/startMsw.ts';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { startMsw } from "./mocks/startMsw.ts";
 
-/** 
- * NEXT_PUBLIC_MSW_ENABLED env 주입된 경우만 실행
-*/
-(() => {
-  const mswEnabled = import.meta.env.NEXT_PUBLIC_MSW_ENABLED === 'true';
-  if (mswEnabled) {
+async function initMsw() {
+  if (import.meta.env.NEXT_PUBLIC_MSW_ENABLED === "true") {
     startMsw();
   }
-})();
-
-
-async function enableMocking() {
-
-
-  const { worker } = await import('./mocks/browser')
-
-  return worker.start()
 }
 
-enableMocking().then(() => {
-  createRoot(document.getElementById('root')!).render(
+initMsw().then(() => {
+  createRoot(document.getElementById("root")!).render(
     <StrictMode>
       <App />
-    </StrictMode>,
-  )
-})
+    </StrictMode>
+  );
+});
--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -2,6 +2,6 @@ import { setupWorker } from 'msw/browser';
 import { handlers } from './handlers';
 
 // This configures a Service Worker with the given request handlers.
-const worker = setupWorker(...handlers);
+export const worker = setupWorker(...handlers);
 
 export default worker;
