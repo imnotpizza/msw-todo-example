57b40091a32a4dbcfd0a5c7c6da4ba38c878c4d0
IMNOTPIZZA <48155178+imnotpizza@users.noreply.github.com>
2026-01-11 20:25:29 +0900

    Test: list api handler 작성

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,3 +1,50 @@
 import { http, HttpResponse } from "msw";
+import type { TodoItem } from "../interfaces";
 
-export const handlers = [];
+const baseUrl = "https://todoserver/v1";
+
+export const handlers = [
+  // GET /api/todos - 할일 목록 조회
+  http.get(baseUrl + "/api/todos", () => {
+    const mockData: TodoItem[] = [
+      {
+        id: 1,
+        name: "MSW 설정하기",
+        checked: "Y",
+      },
+      {
+        id: 2,
+        name: "Todo API 만들기",
+        checked: "Y",
+      },
+      {
+        id: 3,
+        name: "Mock 데이터 작성하기",
+        checked: "N",
+      },
+      {
+        id: 4,
+        name: "UI 컴포넌트 개발하기",
+        checked: "N",
+      },
+      {
+        id: 5,
+        name: "배포 준비하기",
+        checked: "N",
+      },
+    ];
+
+    return HttpResponse.json(mockData, {
+      status: 200,
+    });
+  }),
+
+  // POST /api/todos - 할일 추가
+  http.post<any, TodoItem>(baseUrl + "/api/todos", async ({ request }) => {}),
+
+  // PUT /api/todos/:id - 할일 수정
+  http.put(baseUrl + "/api/todos/:id", async ({ request, params }) => {}),
+
+  // DELETE /api/todos/:id - 할일 삭제
+  http.delete(baseUrl + "/api/todos/:id", ({ params }) => {}),
+];
